8ccccf3329d70aeb2d4076bc80f252e0ace5f795
Thanh Lam <lambtph16545@fpt.edu.vn>
2022-05-26 16:41:05 +0700

    sua giao dien dang ky

--- app/src/main/java/com/example/demo_app_doc_truyen/DangKy_Activity.java
+++ app/src/main/java/com/example/demo_app_doc_truyen/DangKy_Activity.java
@@ -2,28 +2,43 @@ package com.example.demo_app_doc_truyen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.Calendar;
+
 public class DangKy_Activity extends AppCompatActivity {
-    TextInputLayout edtSDT, edtDC, edtHoTen, edtMk, edtMkck;
+    TextInputEditText DateFormat;
+    TextInputLayout edtSDT, edtEmail, edtHoTen, edtMk, edtMkck,edtDate;
     Button btnDK, btnHuy;
     RadioButton rbNam;
     RadioGroup radioGroup;
     Intent intent;
+    DatePickerDialog.OnDateSetListener onDateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
         setTitle("Dang Ky");
         setContentView(R.layout.activity_dang_ky);
-        edtDC = findViewById(R.id.edtDiachi);
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        DateFormat=findViewById(R.id.DateFormat);
+        edtDate= findViewById(R.id.edtDate);
+        edtEmail = findViewById(R.id.edtEmail);
         edtHoTen = findViewById(R.id.edtHoTen);
         edtMk = findViewById(R.id.edtPassDk);
         edtMkck = findViewById(R.id.edtPassCk);
@@ -40,5 +55,24 @@ public class DangKy_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        DateFormat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        DangKy_Activity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        onDateSetListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                datePickerDialog.show();
+
+            }
+        });
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month =month+1;
+                String date = dayOfMonth+"/"+month+"/"+year;
+                edtDate.getEditText().setText(date);
+            }
+        };
     }
 }
